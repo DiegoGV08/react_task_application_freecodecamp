9ce0baa4242ccddea4a58c2fed54cd5d126dde77
Diego Galdamez <diegogaldamez08@gmail.com>
2022-10-17 21:34:59 -0600

    Adding Default Style

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import freeCodeCampLogo from './images/freecodecamp_logo.png'
 
 function App() {
   return (
-    <div className="App">
-      React
+    <div className="task-application">
+      <div className='freecodecamp-logo-container'>
+        <img
+          src={freeCodeCampLogo} alt='this is a logo'
+          className='freecodecamp-logo' />
+      </div>
+
+      <div className='task-list-principal'>
+          <h1>My Tasks</h1>
+      </div>
+
     </div>
   );
 }
